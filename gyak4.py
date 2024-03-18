329a756a24e0a9a98eff66651708aad476cddc5b
HajnalVince <114928119+HajnalVince@users.noreply.github.com>
2024-03-18 18:19:23 +0100

    Add files via upload

---
+++ gyak4.py
@@ -0,0 +1,38 @@
+
+"""
+for szam in range(1,11):
+    if szam % 2 == 0:
+        print(szam)
+"""
+
+
+
+"""
+import random
+
+random_szamok = []
+
+for i in range(5):
+    random_szamok.append(random.randint(1,25))
+    
+    
+osszeg = 0
+
+    
+for szam in random_szamok:
+    osszeg += szam
+    
+print(f"A Számok összege: {osszeg},{random_szamok}")
+"""
+
+
+
+"""
+knev = input("Kérem a keresztnevedet: ")
+
+vnev = input("Kérem a vezetéknevedet: ")
+
+teljes_nev = knev+ " " +vnev
+
+print(f"Üdvözöllek {teljes_nev}")
+"""
